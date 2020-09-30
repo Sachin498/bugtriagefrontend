5913c5fb0f718d4c5fdd7f5a848e43364929749a
Sachin498 <53938517+Sachin498@users.noreply.github.com>
2020-09-30 13:58:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 import sys
 import  numpy as np
 import pickle
+import json
+from collections import defaultdict
+
 
 # Flask
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
@@ -15,15 +18,35 @@ print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(bug_id):
-
+    values = defaultdict()
+    
     infile=open('models/pred','rb')
     x=pickle.load(infile)
-    print(x)
+    #print(x)
     yp=np.argmax(x,axis=1)
     outfile=open('models/cl','rb')
     y=pickle.load(outfile)
-    print(y)
-    return y[yp[int(bug_id)]]
+    
+    jsonFile = open('models/deep_data.json','rb')
+    temp = json.load(jsonFile,strict=False)
+    #print(y)
+    
+    for entry in temp:
+        values[entry['id']] = {
+            'issue_id' : entry['issue_id'],
+            'issue_title' : entry['issue_title'],
+            'reported_time' : entry['reported_time'],
+            'owner' : entry['owner'],
+            'description' : entry['description']
+        }
+      
+    #for key in values.keys():
+    #    print(values[key]['description'])
+    #print(values[bug_id]['description'])
+    nam="The assigned developer is "
+    name = nam+y[yp[int(bug_id)]]
+    desc = "The Bug Description is "+values[bug_id]['description']
+    return (name , desc)
 
 
 @app.route('/', methods=['GET'])
@@ -35,13 +58,12 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == "POST":
-         bug_id = request.form.get('bug_id')
-         devname = model_predict(bug_id)
-         print(type(devname))
-         return render_template('index.html', dev =devname.encode('utf-8').rstrip())
+         bug_id = int(request.form.get('bug_id'))
+         devname,description = model_predict(bug_id)
+         return render_template('index.html', dev =devname, desc=description)
 
 if __name__ == '__main__':
-    app.run(port=5007, threaded=False)
+    app.run(host='localhost',debug=True,threaded=True)
 
     # Serve the app with gevent
     #http_server = WSGIServer(('0.0.0.0', 5000), app)
